dd700a993d9fe8123ea61cdf425f9be25204c452
Frank Migge <public@frank4dd.com>
2016-09-03 19:40:55 +0900

    bugfix and update 20160903

--- plugins/check_tablespace_mysql.java
+++ plugins/check_tablespace_mysql.java
@@ -1,60 +1,83 @@
 // ----------------------------------------------------------------------------
-// check_tablespace_mysql.java 20100922 frank4dd version 1.0
+// check_tablespace_mysql.java 20160903 frank4dd version 1.2
 // ----------------------------------------------------------------------------
 // e-mail: support[at]frank4dd.com
 // web:    http://nagios.fm4dd.com/
 //
-// This nagios plugin queries the information_schema.TABLES system table.
-// Supported are MySQL Server versions 5.0 and up.
-// MySQL's default storage engine MyISAM has no concept of tablespaces,
-// 3 files are generated per table and can only be placed together per DB
-// using the 'data-dir' path in the underlying filesystem. For our purpose,
-// we handle it as if there is exactly one tablespace and sum up
-// all datafile sizes for a given database.
-// Consequently, there is no tablespace name parameter or list
-// of available tablespaces.
+// This nagios plugin queries the size in information_schema.TABLES system table.
+// Supported are MySQL Server versions 5.0 and up with engines MyISAM, InnoDB,
+// and TocuDB.
+//
+// MyISAM: MySQL's default storage engine has no concept of tablespaces.
+// We handle it as if there is exactly one tablespace and sum up all
+// table sizes for a given database schema.
 //
 // Pre-requisites: MySQL JDBC driver installed and a valid DB user.
 // ----------------------------------------------------------------------------
 // Example Output:
-// > java check_tablespace_mysql 192.168.0.1  3306 edacs root Pillnitz1 -r
-// Tablespace OK: edacs 4961 KBytes|edacs: 27 datafiles, 4961 KB
-// > java check_tablespace_mysql 192.168.0.1  3306 edacs root Pillnitz1 -d
-// DB connect: jdbc:mysql://127.0.0.1:3306/edacs?user=root&password=Pillnitz1
+// > java check_tablespace_mysql 192.168.0.1  3306 edacs root Secret1 -r
+// Tablespace OK - 59.88 MB used|bytes_used=62783488;; datafiles=30
+//
+// > java check_tablespace_mysql 192.168.0.1  3306 edacs root Secret1 -d
+// DB connect: jdbc:mysql://127.0.0.1:3306/edacs?user=root&password=Secret1
 // File Name:       edacs_daystats Space used:         11 KB
 // File Name:        edacs_mainlog Space used:       1520 KB
-// File Name:       edacs_monstats Space used:          2 KB
-// File Name:         edacs_remote Space used:         43 KB
-// File Name:         edacs_router Space used:          2 KB
-// File Name:        edacs_service Space used:          2 KB
-// File Name:        edacs_templog Space used:       3364 KB
-// File Name:          edacs_users Space used:         15 KB
+// ...
 // File Name:        edacs_version Space used:          2 KB
-// > java check_tablespace_mysql 192.168.0.1  3306 edacs root Pillnitz1 4000 5000
-// Tablespace WARN: edacs 4961 KBytes|edacs: 27 datafiles, 4961 KB
+//
+// > java check_tablespace_mysql 192.168.0.1  3306 edacs root Secret1 50000000 70000000
+// Tablespace WARN - 59.88 MB used|bytes_used=62783488;; datafiles=30
+//
+// Revision history:
+// 20100922 initial release version 1.0
+// 20160902 bugfix: type assignment (MySQL bigint->Java int) changed to Long
+// 20160903 base count changed to bytes, adding function for human readable format
+//          skip calculation for views, and change file count to table count
 
 import java.sql.*;
+import java.text.*;
 
 class check_tablespace_mysql {
 
-  static int kbytes_warn = 0;  // the commandline argument for warning threshold of KB used
-  static int kbytes_crit = 0;  // the commandline argument for critical threshold of KB used
-  static int dfiles_total= 0;  // the returned number of tablespace files
-  static int kbytes_used = 0;  // the returned tablespace value of used KB
-  static int kbytes_total= 0;  // the returned tablespace value of total KB available
-  static int percent_used= 0;  // the returned tablespace value, current space used in percent
-  static int return_code = 0;  // 'OK'=>0,'WARNING'=>1,'CRITICAL'=>2,'UNKNOWN'=>3,'DEPENDENT'=>4
-  static int debug       = 0;  // 'normal'=>0,'verbose'=>1 when -d parameter is given
-  static String output   = ""; // the plugin output string
-  static String perfdata = ""; // the plugin perfdata output, returning the KB values
-  static String tbspname = ""; // the tablespace to check
-  static String dbUrl    = ""; // the access URL for the database to query
-  static String query    = ""; // the SQL query to execute
+  static long bytes_warn  = 0;  // the commandline argument for warning threshold of bytes used
+  static long bytes_crit  = 0;  // the commandline argument for critical threshold of bytes used
+  static long bytes_used  = 0;  // the returned tablespace summary of used bytes
+  static int  table_total = 0;  // the returned number of tables
+  static int debug        = 0;  // 'normal'=>0,'verbose'=>1 when -d parameter is given
+  static String tbltype   = ""; // the MySQL table type, e.g. "BASE TABLE", "VIEW"
+  static String output    = ""; // the plugin output string
+  static String perfdata  = ""; // the plugin perfdata output, returning bytes_used and # of datafiles
+  static String tbspname  = ""; // the MySQL database to check
+  static String dbUrl     = ""; // the access URL for the database to query
+  static String query     = ""; // the SQL query to execute
+
+  public static String floatForm (double d) {
+     return new DecimalFormat("0.00").format(d);
+  }
+
+  public static String bytesToHuman (long size) {
+    long Kb = 1  * 1024;
+    long Mb = Kb * 1024;
+    long Gb = Mb * 1024;
+    long Tb = Gb * 1024;
+    long Pb = Tb * 1024;
+    long Eb = Pb * 1024;
+
+    if (size <  Kb)                 return floatForm(size) + " Bytes";
+    if (size >= Kb && size < Mb)    return floatForm((double)size / Kb) + " KB";
+    if (size >= Mb && size < Gb)    return floatForm((double)size / Mb) + " MB";
+    if (size >= Gb && size < Tb)    return floatForm((double)size / Gb) + " GB";
+    if (size >= Tb && size < Pb)    return floatForm((double)size / Tb) + " TB";
+    if (size >= Pb && size < Eb)    return floatForm((double)size / Pb) + " PB";
+    if (size >= Eb)                 return floatForm((double)size / Eb) + " EB";
+
+    return "Out of bounds"; // return overflow error, we won't calculate beyond EB
+  }
 
   public static void main (String args[]) {
     if (args.length < 6) {
       System.err.println("Error: Missing Arguments.");
-      System.err.println("Syntax: java check_tablespace_mysql <db-ip> <db-port> <db-instance> <db-user> <db-pwd> <kbytes-warn> <kbytes-crit>");
+      System.err.println("Syntax: java check_tablespace_mysql <db-ip> <db-port> <db-instance> <db-user> <db-pwd> <bytes-warn> <bytes-crit>");
       System.err.println("        java check_tablespace_mysql <db-ip> <db-port> <db-instance> <db-user> <db-pwd> -r");
       System.err.println("        java check_tablespace_mysql <db-ip> <db-port> <db-instance> <db-user> <db-pwd> -d");
       System.exit(-1);
@@ -71,71 +94,86 @@ class check_tablespace_mysql {
 
     // Check if we got warn and crit values to check against
     if (args.length == 7) {
-      kbytes_warn = Integer.parseInt(args[5]);
-      kbytes_crit = Integer.parseInt(args[6]);
+      bytes_warn = Integer.parseInt(args[5]);
+      bytes_crit = Integer.parseInt(args[6]);
     }
+
+    // assign schema name
+    tbspname = args[2];
 
     try {
       Class.forName("com.mysql.jdbc.Driver");
     } catch (ClassNotFoundException e) {
       System.err.println("Error: JDBC Driver Problem.");
       System.err.println (e);
-      
-      System.exit (3); // return UNKNOWN
+      System.exit(3); // return UNKNOWN
     }
     try {
       // open connection to database "jdbc:mysql://destinationhost:port/dbname?dbuser&dbpassword"
       Connection connection = DriverManager.getConnection(dbUrl);
 
-      // build query, here we use table "information_schema.TABLES"
-      // size is returned in bytes, we convert it into KBytes
-      String query = "SELECT table_name, round (data_length + index_length)/(1024) 'KB size' FROM information_schema.TABLES WHERE ENGINE=('MyISAM' || 'InnoDB' ) AND table_schema = '" + args[2] + "'";
+      // (1) query schema size in bytes from table "information_schema.TABLES"
+      String query = "SELECT table_name, table_type, round (data_length + index_length) 'B size' FROM information_schema.TABLES WHERE table_schema = '" + tbspname + "'";
 
       // execute query
       Statement statement = connection.createStatement () ;
-      ResultSet rs = statement.executeQuery (query) ;
+      ResultSet schema_sz = statement.executeQuery (query) ;
 
       // Loop through the result set
-      while( rs.next() ) {
-        // get content from column "1-2"
+      while( schema_sz.next() ) {
+        // display debug output if called with "-r" report parameter
         if (debug == 1) {
-          System.out.format ("File Name: %20s ", rs.getString (1)); // PHYSICAL_NAME, NVARCHAR(260)
-          System.out.format ("Space used: %10d KB\n", rs.getInt(2)); // SIZE, INT, Current filesize in 8-KB pages
+          System.out.format ("Table Name: %-25s ", schema_sz.getString(1)); // TABLE_NAME, varchar(64)
+          System.out.format ("Table Type: %-15s ", schema_sz.getString(2)); // TABLE_TYPE, varchar(64)
+          System.out.format ("Space used: %-15s\n", bytesToHuman(schema_sz.getLong(3))); // SIZE, BIGINT(unsigned)
         }
 
-        kbytes_used = kbytes_used + rs.getInt(2);
-        dfiles_total = dfiles_total+3;
-      }
+        tbltype = schema_sz.getString(2);
 
-      rs.close () ;
-      statement.close () ;
-      connection.close () ;
+        // If the returned row is not a real table (e.g. a view), skip here
+        if(! tbltype.contains("TABLE")) continue;
+
+        // If the sum is going to overflow the datatype, create error
+        if(bytes_used + schema_sz.getLong(3) > Long.MAX_VALUE) {
+          System.out.println("Error: calculation overflow.|");
+          System.exit(3); // Unknown
+        }
+        else {
+          bytes_used = bytes_used + schema_sz.getLong(3);
+        }
+       
+        table_total = table_total + 1;
+      }
+      schema_sz.close();
+      statement.close();
+      connection.close();
 
     } catch (java.sql.SQLException e) {
-      System.err.println (e) ;
-      System.exit (3) ; // Unknown
+      System.err.println("Error: SQL Problem.");
+      System.err.println(e) ;
+      System.exit(3); // Unknown
     }
 
-    perfdata = args[2] + ": " + dfiles_total + " datafiles, " + kbytes_used + " KB";
-    output = args[2] + " " + kbytes_used + " KBytes" + "|" + perfdata;
+    perfdata ="bytes_used=" +  bytes_used + ";; table_count=" + table_total;
+    output = bytesToHuman(bytes_used) + " used|" + perfdata;
 
-    if ( (kbytes_warn != 0) && (kbytes_crit != 0) ) {
-      if ( kbytes_used < kbytes_warn ) {
-        System.out.println("Tablespace OK: " + output);
-        System.exit (0); // OK
+    if ( (bytes_warn != 0) && (bytes_crit != 0) ) {
+      if ( bytes_used < bytes_warn ) {
+        System.out.println("OK - " + tbspname + " " + output);
+        System.exit(0); // OK
       }
-      if ( kbytes_used >= kbytes_warn && kbytes_used < kbytes_crit ) {
-        System.out.println("Tablespace WARN: "  + output);
-        System.exit (1); // WARN
+      if ( bytes_used >= bytes_warn && bytes_used < bytes_crit ) {
+        System.out.println("WARN - " + tbspname + " " + output);
+        System.exit(1); // WARN
       }
-      if ( kbytes_used  >= kbytes_crit ) {
-        System.out.println("Tablespace CRIT: "  + output);
-        System.exit (2); // CRIT
+      if ( bytes_used  >= bytes_crit ) {
+        System.out.println("CRIT - "   + tbspname + " " + output);
+        System.exit(2); // CRIT
       }
     }
     if (args.length == 6 && args[5].equals("-r")) {
-      System.out.println("Tablespace OK: " + output);
-      System.exit (0); // OK
+      System.out.println("OK - " + tbspname + " " + output);
+      System.exit(0); // OK
     }
   }
 }
--- plugins/check_tablespace_mysql.md
+++ plugins/check_tablespace_mysql.md
@@ -6,7 +6,7 @@ Copyright (c) 2010 Frank4DD<support[at]frank4dd.com>
 
 ### check_tablespace_mysql
 
-This plugin checks the tablespace size of a specific MySQL Server database against WARN and CRIT thresholds. It returns the current tablespace size and the number of data files belonging to this database. The plugin can be called in 'reporting' mode, returning the space values withouth checking against a threshold. This is helpful if the tablespace only needs to be graphed as a trend over time.
+This plugin checks the schema size of a specific MySQL Server database against WARN and CRIT thresholds. It returns the current database size and the number of tables belonging to this database. The plugin can be called in 'reporting' mode, returning the space values withouth checking against a threshold. This is helpful if the size only needs to be graphed as a trend over time.
 
 It requires the database to be set up for accepting network connections and being reachable through that network port from Nagios. The plugin uses the MySQL JDBC driver, this driver must be installed and found through the Java classpath on the server executing this plugin. [(JDBC installation example)](http://fm4dd.com/database/howto-install-MySQL-jdbc.htm)
 
@@ -49,27 +49,28 @@ It requires the database to be set up for accepting network connections and bein
 The plugin in 'reporting' mode, returns OK if the tablespace size could be fetched.
 
 <pre># java -classpath /srv/app/nagios/libexec/ check_tablespace_mysql 192.168.98.128 3306 edacs "edacsread" "dbpass" -r
-Tablespace OK: edacs 4961 KBytes|edacs: 27 datafiles, 4961 KB</pre>
+OK - edacs 59.88 MB used|bytes_used=62783488;; table_count=9</pre>
 
 The plugin in 'check' mode, returns the status depending on the tablespace size exceeding the WARN and CRIT threshold values.
 
 <pre># java -classpath /srv/app/nagios/libexec/ check_tablespace_mysql 192.168.98.128 3306 edacs "edacsread" "dbpass"
- 4000 5000
-Tablespace WARN: edacs 4961 KBytes|edacs: 27 datafiles, 4961 KB</pre>
+ 40000000 70000000
+WARN - Schema edacs 59.88 MB used|bytes_used=62783488;; table_count=9</pre>
 
-The plugin in 'debug' mode, showing individual data file sizes for this database.
+The plugin in 'debug' mode, showing individual table sizes for this database.
 
 <pre># java -classpath /srv/app/nagios/libexec/ check_tablespace_mysql 192.168.98.128 3306 edacs "edacsread" "dbpass" -d
 DB connect: jdbc:mysql://192.168.98.128:3306/edacs?user=edacsread&password=dbpass
-File Name:       edacs_daystats Space used:         11 KB
-File Name:        edacs_mainlog Space used:       1520 KB
-File Name:       edacs_monstats Space used:          2 KB
-File Name:         edacs_remote Space used:         43 KB
-File Name:         edacs_router Space used:          2 KB
-File Name:        edacs_service Space used:          2 KB
-File Name:        edacs_templog Space used:       3364 KB
-File Name:          edacs_users Space used:         15 KB
-File Name:        edacs_version Space used:          2 KB</pre>
+Table Name: edacs_daystats            Table Type: BASE TABLE      Space used: 80.00 KB
+Table Name: edacs_mainlog             Table Type: BASE TABLE      Space used: 26.70 MB
+Table Name: edacs_monstats            Table Type: BASE TABLE      Space used: 16.00 KB
+Table Name: edacs_remote              Table Type: BASE TABLE      Space used: 352.00 KB
+Table Name: edacs_router              Table Type: BASE TABLE      Space used: 16.00 KB
+Table Name: edacs_service             Table Type: BASE TABLE      Space used: 16.00 KB
+Table Name: edacs_templog             Table Type: BASE TABLE      Space used: 32.64 MB
+Table Name: edacs_users               Table Type: BASE TABLE      Space used: 48.00 KB
+Table Name: edacs_version             Table Type: BASE TABLE      Space used: 16.00 KB
+Table Name: v_edacs                   Table Type: VIEW            Space used: 0.00 Bytes</pre>
 
 Nagios Graph:
 
